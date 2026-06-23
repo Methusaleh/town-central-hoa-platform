f0c00665d74f63712a9a0f34093b6afefbf18f3d
Aaron Kipf <samplethis84@gmail.com>
2026-06-23 14:31:01 -0500

    edits to index.js and index.html to allow up to 2mb avatars

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,9 @@ app.use(
   }),
 );
 
-app.use(express.json());
+// Allows our base64 image streams to pass through smoothly up to 2MB
+app.use(express.json({ limit: "2mb" }));
+app.use(express.urlencoded({ limit: "2mb", extended: true }));
 
 // 2. ADD THIS: Plug in the announcements route
 app.use("/api/announcements", announcementRoutes);
