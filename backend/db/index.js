ef952f3f2694ce14d2921de0f22a7051995666e5
Aaron Kipf <samplethis84@gmail.com>
2026-09-05 14:42:25 -0500

    added idle timeout for my neon db so it will not stay open.

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  max: 5,                  // Limits max concurrent connections to keep pool lightweight
+  idleTimeoutMillis: 30000, // Closes idle clients after 30 seconds of inactivity
+  connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
 });
 
 module.exports = {
